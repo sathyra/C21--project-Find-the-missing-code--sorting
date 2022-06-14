452cc07cfb70d78f7ce2da0a1358b8c1e8c8c363
sathyra <105861021+sathyra@users.noreply.github.com>
2022-06-14 14:35:16 +0530

    Add files via upload

---
+++ act.py
@@ -0,0 +1,35 @@
+import numpy as np
+from tkinter import *
+root=Tk()
+root.geometry("600x400")
+arr1 = np.array([3, 2, 0, 1])
+arr2 = np.array(['banana', 'cherry', 'apple'])
+
+#Defining the sorting function
+def srt_ar_asc():
+    #write a code to sort in ascending
+    newarr3 = np.sort(arr1)
+    label.config(text=newarr3)
+
+def srt_ar_dsc():
+    #write a code to sort in descending
+    label.config(text=newarr3)
+
+def srt_txt_asc():
+    #write a code to sort the text 
+    label.config(text=newarr4)
+
+#display the array elements
+Message(root, text= arr1,bg='light blue', font = 50).pack()
+
+#Write a code to create a ascending button
+
+#Write a code to create a descending button
+
+#Button and text elements
+Message(root, text= arr2,bg='light blue', font = 50).pack()
+Button(root, text="Sort Text", command=srt_txt_asc).pack(pady=20)
+
+#Write a code to display the output using the label
+label.pack()
+root.mainloop()
